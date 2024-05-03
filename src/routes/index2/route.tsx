9a2d715f2cef8c8e725de2d9fd21522c463b9418
Said_Hammar <saidhammar15@gmail.com>
2024-05-02 23:50:17 -0400

    Carousel added

--- src/routes/index2/route.tsx
+++ src/routes/index2/route.tsx
@@ -1,11 +1,17 @@
 // routes/_index2/route.tsx
 import { RouteComponent } from "@remix-run/react/dist/routeModules";
+import Carousel from "../../../src/components/Carousel.tsx";
+import { ReactNode } from "react";
 
-const Index2Page: RouteComponent = () => {
+const Index2Page: RouteComponent = ({ children }: { children?: ReactNode }) => {
   return (
     <main className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-3xl font-bold mb-4">Bienvenue sur Index2</h1>
       <p className="text-lg">Merci de visiter notre page d'accueil.</p>
+      <div>
+      <Carousel />
+      {children}
+      </div>
     </main>
   );
 };
